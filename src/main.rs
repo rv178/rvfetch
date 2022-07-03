d5dee0d4cfc74980e0d17d957ff1d294cd51c174
rv178 <idliyout@gmail.com>
2022-07-03 21:02:56 +0530

    [update] increase length of start_bar and end_bar

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ fn main() {
         format!("{}@{}", env::var("USER").unwrap().green(), sys.host_name().unwrap().green());
 
     // start bar
-    let start_bar: String = "┌────────────────────────────────┐".bright_black().to_string();
+    let start_bar: String = "┌──────────────────────────────────┐".bright_black().to_string();
 
     // distro name
     let sys_name: String = format!("{}      - {}", "OS".blue().bold(), sys.name().unwrap().bold());
@@ -41,7 +41,7 @@ fn main() {
     let mem: String = format!("{}  - {}", "Memory".blue().bold(), mem_str.bold());
 
     // end bar
-    let end_bar: String = "└────────────────────────────────┘".bright_black().to_string();
+    let end_bar: String = "└──────────────────────────────────┘".bright_black().to_string();
 
     // vec containing each line of moon_art
     let moon_art: Vec<&str> = moon_art.split("\n").collect::<Vec<&str>>();
