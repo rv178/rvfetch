1b74974126a30d26aaf091b669ddaf5d0a079289
Idlidev <idliyout@gmail.com>
2022-07-03 18:07:06 +0530

    [update] add comments
    * main.rs: make the code more readable

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use sysinfo::{System, SystemExt};
 fn main() {
     let mut sys = System::new_all();
     sys.refresh_all();
+
+    // art
     let moon_art = format!(
         "     _.._          
   .' .-'`       
@@ -14,20 +16,38 @@ fn main() {
   '._  _.'        
                 "
     );
-    let sys_name = format!("{}      - {}", "OS".blue().bold(), sys.name().unwrap().bold());
-    let bar1 = "┌────────────────────────────────┐".bright_black();
-    let bar2 = "└────────────────────────────────┘".bright_black();
-    let sys_kern =
-        format!("{}  - {}", "Kernel".blue().bold(), sys.kernel_version().unwrap().bold());
-    let shell = format!("{}   - {}", "Shell".blue().bold(), env::var("SHELL").unwrap().bold());
-    let mem_str = format!("{} MB / {} MB", sys.used_memory() / 1000, sys.total_memory() / 1000);
-    let mem = format!("{}  - {}", "Memory".blue().bold(), mem_str.bold());
-    let hostname =
+
+    // user@hostname
+    let hostname: String =
         format!("{}@{}", env::var("USER").unwrap().green(), sys.host_name().unwrap().green());
 
-    let moon_art = moon_art.split("\n").collect::<Vec<&str>>();
-    let fields: Vec<String> =
-        vec![hostname, bar1.to_string(), sys_name, sys_kern, shell, mem, bar2.to_string()];
+    // start bar
+    let start_bar: String = "┌────────────────────────────────┐".bright_black().to_string();
+
+    // distro name
+    let sys_name: String = format!("{}      - {}", "OS".blue().bold(), sys.name().unwrap().bold());
+
+    // kernel version
+    let sys_kern: String =
+        format!("{}  - {}", "Kernel".blue().bold(), sys.kernel_version().unwrap().bold());
+
+    // shell name
+    let shell: String =
+        format!("{}   - {}", "Shell".blue().bold(), env::var("SHELL").unwrap().bold());
+
+    // memory used (mb) / total memory (mb)
+    let mem_str: String =
+        format!("{} MB / {} MB", sys.used_memory() / 1000, sys.total_memory() / 1000);
+    let mem: String = format!("{}  - {}", "Memory".blue().bold(), mem_str.bold());
+
+    // end bar
+    let end_bar: String = "└────────────────────────────────┘".bright_black().to_string();
+
+    // vec containing each line of moon_art
+    let moon_art: Vec<&str> = moon_art.split("\n").collect::<Vec<&str>>();
+
+    // all fields
+    let fields: Vec<String> = vec![hostname, start_bar, sys_name, sys_kern, shell, mem, end_bar];
 
     for i in 0..moon_art.len() {
         print!("{}", moon_art[i].yellow());
