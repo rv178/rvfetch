74ffbbb4d73f3aee3502c44eaaa89fbfd90c6f35
rv178 <idliyout@gmail.com>
2022-07-11 12:55:25 +0530

    [refactor] codebase

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
-use colored::Colorize;
 use std::env;
 use sysinfo::{System, SystemExt};
 
 fn main() {
+    let gra = "\x1b[38;5;8m";
+    let gre = "\x1b[32m";
+    let yel = "\x1b[33m";
+    let blu = "\x1b[34m";
+    let bld = "\x1b[1m";
+    let res = "\x1b[0m";
+
+    let mut fields: Vec<String> = Vec::new();
+
+    macro_rules! field {
+        ($x:expr, $y:expr) => {
+            fields.push(format!("{}{}{} - {}{}{}", blu, $x, res, bld, $y, res))
+        };
+    }
+
     let mut sys = System::new_all();
     sys.refresh_all();
 
-    // art
+    let mem_str = format!("{} MB / {} MB", sys.used_memory() / 1000, sys.total_memory() / 1000);
     let moon_art = format!(
         "     _.._          
   .' .-'`       
@@ -16,41 +30,25 @@ fn main() {
   '._  _.'        
                 "
     );
-
-    // user@hostname
-    let hostname: String =
-        format!("{}@{}", env::var("USER").unwrap().green(), sys.host_name().unwrap().green());
-
-    // start bar
-    let start_bar: String = "┌──────────────────────────────────┐".bright_black().to_string();
-
-    // distro name
-    let sys_name: String = format!("{}      - {}", "OS".blue().bold(), sys.name().unwrap().bold());
-
-    // kernel version
-    let sys_kern: String =
-        format!("{}  - {}", "Kernel".blue().bold(), sys.kernel_version().unwrap().bold());
-
-    // shell name
-    let shell: String =
-        format!("{}   - {}", "Shell".blue().bold(), env::var("SHELL").unwrap().bold());
-
-    // memory used (mb) / total memory (mb)
-    let mem_str: String =
-        format!("{} MB / {} MB", sys.used_memory() / 1000, sys.total_memory() / 1000);
-    let mem: String = format!("{}  - {}", "Memory".blue().bold(), mem_str.bold());
-
-    // end bar
-    let end_bar: String = "└──────────────────────────────────┘".bright_black().to_string();
-
-    // vec containing each line of moon_art
     let moon_art: Vec<&str> = moon_art.split("\n").collect::<Vec<&str>>();
 
-    // all fields
-    let fields: Vec<String> = vec![hostname, start_bar, sys_name, sys_kern, shell, mem, end_bar];
+    fields.push(format!(
+        "{}{}@{}{}",
+        gre,
+        env::var("USER").unwrap(),
+        sys.host_name().unwrap(),
+        res
+    ));
+
+    fields.push(format!("{}┌──────────────────────────────────┐{}", gra, res));
+    field!("OS", sys.name().unwrap());
+    field!("KR", sys.kernel_version().unwrap());
+    field!("SH", env::var("SHELL").unwrap());
+    field!("ME", mem_str);
+    fields.push(format!("{}└──────────────────────────────────┘{}", gra, res));
 
     for i in 0..moon_art.len() {
-        print!("{}", moon_art[i].yellow());
+        print!("{}{}{}", yel, moon_art[i], res);
         print!("{}", fields[i]);
 
         print!("\n");
