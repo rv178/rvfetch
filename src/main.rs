f045e06600bce167f42a75c0d77191620ab6adf7
rv178 <idliyout@gmail.com>
2022-07-11 13:11:29 +0530

    [update] use icons

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,17 @@ use std::env;
 use sysinfo::{System, SystemExt};
 
 fn main() {
-    let gra = "\x1b[38;5;8m";
-    let gre = "\x1b[32m";
-    let yel = "\x1b[33m";
-    let blu = "\x1b[34m";
-    let bld = "\x1b[1m";
-    let res = "\x1b[0m";
+    let gray = "\x1b[38;5;8m";
+    let green = "\x1b[32m";
+    let yellow = "\x1b[33m";
+    let blue = "\x1b[34m";
+    let reset = "\x1b[0m";
 
     let mut fields: Vec<String> = Vec::new();
 
     macro_rules! field {
         ($x:expr, $y:expr) => {
-            fields.push(format!("{}{}{} - {}{}{}", blu, $x, res, bld, $y, res))
+            fields.push(format!("  {}{}{} : {}", blue, $x, reset, $y))
         };
     }
 
@@ -22,33 +21,36 @@ fn main() {
 
     let mem_str = format!("{} MB / {} MB", sys.used_memory() / 1000, sys.total_memory() / 1000);
     let moon_art = format!(
-        "     _.._          
-  .' .-'`       
- /  /             
- |  |             
- \\  '.___.;       
-  '._  _.'        
-                "
+        "     _.._\t
+  .' .-'`\t
+ /  /     \t
+ |  |     \t
+ \\  '.___.;  \t
+  '._  _.' \t
+\t\t"
     );
     let moon_art: Vec<&str> = moon_art.split("\n").collect::<Vec<&str>>();
 
-    fields.push(format!(
-        "{}{}@{}{}",
-        gre,
+    let hostname = format!(
+        "{}{}{}@{}{}{}",
+        green,
         env::var("USER").unwrap(),
+        reset,
+        green,
         sys.host_name().unwrap(),
-        res
-    ));
+        reset
+    );
 
-    fields.push(format!("{}┌──────────────────────────────────┐{}", gra, res));
-    field!("OS", sys.name().unwrap());
-    field!("KR", sys.kernel_version().unwrap());
-    field!("SH", env::var("SHELL").unwrap());
-    field!("ME", mem_str);
-    fields.push(format!("{}└──────────────────────────────────┘{}", gra, res));
+    field!(" ", hostname);
+    fields.push(format!("{}┌──────────────────────────────────┐{}", gray, reset));
+    field!(" ", sys.name().unwrap());
+    field!(" ", sys.kernel_version().unwrap());
+    field!(" ", env::var("SHELL").unwrap());
+    field!(" ", mem_str);
+    fields.push(format!("{}└──────────────────────────────────┘{}", gray, reset));
 
     for i in 0..moon_art.len() {
-        print!("{}{}{}", yel, moon_art[i], res);
+        print!("{}{}{}", yellow, moon_art[i], reset);
         print!("{}", fields[i]);
 
         print!("\n");
